d05104d50db2965c6f4770d2a23ae9959942498b
Sofia Cividini <SofiaIT@users.noreply.github.com>
2014-05-22 09:32:29 +0200

    plot3.R (ASSIGNMENT 2)

---
+++ plot3.R
@@ -0,0 +1,62 @@
+####################
+#   ASSIGNMENT 2   #
+####################
+
+
+# I set my working directory.
+
+setwd("C:/Documents and Settings/Sofia Cividini/Desktop")
+
+# I open the "Source_Classification_Code.rds" dataset in reading.
+
+source <- readRDS("Source_Classification_Code.rds")
+source <- edit(source)
+
+# I open the "summarySCC_PM25.rds" dataset in reading.
+
+NEI <- readRDS("summarySCC_PM25.rds")
+
+# I subset NEI dataset into the following four datasets of my interest.
+
+NEI.1999 <- subset(NEI, year==1999)
+NEI.2002 <- subset(NEI, year==2002)
+NEI.2005 <- subset(NEI, year==2005)
+NEI.2008 <- subset(NEI, year==2008)
+
+# I merge the datasets above into a unique dataset called NEI.total.
+
+NEI.total <- rbind(NEI.1999, NEI.2002, NEI.2005, NEI.2008)
+
+# I select only Baltimore City, Maryland
+
+NEI.Baltimore <- subset(NEI.total, fips=="24510")
+
+
+#°°°°°°°°°°°°#
+# QUESTION 3 #
+#°°°°°°°°°°°°#
+
+# Of the four types of sources indicated by the type variable, which of these
+# four sources have seen decreases in emissions from 1999-2008 for Baltimore
+# City ? Which have seen increases in emissions from 1999-2008 ?
+
+# I make a plot by using the R's ggplot2 plotting system to answer this question.
+
+# I open ggplots library and I go to see factor label information.
+
+library(ggplot2)
+str(NEI.Baltimore)
+
+# I make a plot by group (point, nonpoint, onroad, nonroad) by using facets.
+
+qplot(year, Emissions, data=NEI.Baltimore, facets = .~ type)
+
+
+## I create the file.png
+
+png(filename = "plot3.png", width = 480, height = 480)
+
+qplot(year, Emissions, data=NEI.Baltimore, facets = .~ type)
+
+dev.off()
+
