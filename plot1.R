e79d599e0cd5e294dc5d6c7e7dd9183d5d1d71ff
Sofia Cividini <SofiaIT@users.noreply.github.com>
2014-05-22 14:24:11 +0200

    plot1.R (ASSIGNMENT 2)

--- plot1.R
+++ plot1.R
@@ -18,10 +18,10 @@ NEI <- readRDS("summarySCC_PM25.rds")
 
 # I subset NEI dataset into the following four datasets of my interest.
 
-NEI.1999 <- subset(NEI, year==1999)
-NEI.2002 <- subset(NEI, year==2002)
-NEI.2005 <- subset(NEI, year==2005)
-NEI.2008 <- subset(NEI, year==2008)
+NEI.1999 <- subset(NEI, year=="1999")
+NEI.2002 <- subset(NEI, year=="2002")
+NEI.2005 <- subset(NEI, year=="2005")
+NEI.2008 <- subset(NEI, year=="2008")
 
 # I merge the datasets above into a unique dataset called NEI.total.
 
