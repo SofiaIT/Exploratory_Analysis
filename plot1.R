3a4ea767fff8928563240ff56e348082164ab3bf
Sofia Cividini <SofiaIT@users.noreply.github.com>
2014-05-22 08:23:29 +0200

    plot1.R (ASSIGNMENT 2)

---
+++ plot1.R
@@ -0,0 +1,51 @@
+###################
+#   ASSIGNMENT 2   #
+####################
+
+
+# I set my working directory.
+
+setwd("C:/Documents and Settings/Sofia Cividini/Desktop")
+
+# I open the "Source_Classification_Code.rds" dataset in reading.
+
+source <- readRDS("Source_Classification_Code.rds")
+source <- edit(source)
+
+# I open the "summarySCC_PM25.rds" dataset in reading.
+
+NEI <- readRDS("summarySCC_PM25.rds")
+
+# I subset NEI dataset into the following four datasets of my interest.
+
+NEI.1999 <- subset(NEI, year==1999)
+NEI.2002 <- subset(NEI, year==2002)
+NEI.2005 <- subset(NEI, year==2005)
+NEI.2008 <- subset(NEI, year==2008)
+
+# I merge the datasets above into a unique dataset called NEI.total.
+
+NEI.total <- rbind(NEI.1999, NEI.2002, NEI.2005, NEI.2008)
+
+
+#°°°°°°°°°°°°#
+# QUESTION 1 #
+#°°°°°°°°°°°°#
+
+# Have total emission from PM2.5 decreased in the United States from 1999 to
+# 2008 ?
+
+# I create a plot with the base plotting system to reply to this question.
+
+plot(Emissions ~ year, data=NEI.total, type="l", ylab="Emissions of PM2.5", xlab="Years",
+main="Total PM2.5 emission")
+
+
+## I create the file.png
+
+png(filename = "plot1.png", width = 480, height = 480)
+
+plot(Emissions ~ year, data=NEI.total, type="l", ylab="Emissions of PM2.5", xlab="Years",
+main="Total PM2.5 emission")
+
+dev.off()
