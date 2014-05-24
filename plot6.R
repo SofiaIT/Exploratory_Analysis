b161caf1a05a16dd7c24cdff80ab4fe32e91cb12
Sofia Cividini <SofiaIT@users.noreply.github.com>
2014-05-24 10:24:06 +0200

    plot6.R (ASSIGNMENT 2)

--- plot6.R
+++ plot6.R
@@ -20,14 +20,12 @@ NEI <- readRDS("summarySCC_PM25.rds")
 
 factor(NEI$type)
 
-
 # I subset NEI dataset into the following four datasets of my interest.
 
 NEI.1999 <- subset(NEI, year == "1999")
 NEI.2002 <- subset(NEI, year == "2002")
 NEI.2005 <- subset(NEI, year == "2005")
 NEI.2008 <- subset(NEI, year == "2008")
-
 
 # I merge the datasets above into a unique dataset called NEI.total.
 
@@ -38,11 +36,9 @@ row.names(NEI.total) <- NULL
 
 NEI.Baltimore <- subset(NEI.total, fips == "24510")
 
-
 # I select Los Angeles County, California.
 
 NEI.LosAngeles <- subset(NEI.total, fips == "06037")
-
 
 # I merge the two datasets above into a unique dataset called NEI.cities.
 
@@ -54,12 +50,18 @@ row.names(NEI.cities) <- NULL
 NEI.cities <- subset(NEI.cities, type == "ON-ROAD")
 row.names(NEI.cities) <- NULL
 
+# I omit the missing values from my data set.
 
-#°°°°°°°°°°°°°°°°#
+NEI.cities <- na.omit(NEI.cities)
+
+
+#°°°°°°°°°°°°#
 # QUESTION 6 #
-#°°°°°°°°°°°°°°°°#
+#°°°°°°°°°°°°#
 
-# Compare emissions from motor vehicle sources between Baltimore City and Los Angeles County over time.
+# Compare emissions from motor vehicle sources in Baltimore City with emissions
+# from motor vehicle sources in Los Angeles County, California.
+# Which city has seen greater changes over time in motor vehicle emissions ?
 
 # I make a plot by using the R's ggplot2 plotting system to answer this question.
 
@@ -68,21 +70,28 @@ row.names(NEI.cities) <- NULL
 library(ggplot2)
 str(NEI.cities)
 
+# I make a basic plot in order to see the change of the PM2.5 emissions from motor vehicles 
+# over time within each city.
 
-# I make a basic plot in order to see the change of the PM2.5 emissions from motor vehicles over time within each city.
+g <- ggplot(NEI.cities, aes(x=log(Emissions), y=type))
 
-g <- ggplot(NEI.cities, aes(Emissions, type))
-g + geom_point() + facet_grid(. ~ fips) + geom_smooth(method = "lm") + 
-labs(title = "Baltimore City vs Los Angeles County") + labs(x = "PM2.5 Emission", y = "Motor Vehicle Sources") 
+summary(g)
+
+g  +  geom_point(color = "steelblue", size = 4, alpha = 1/2) +  
+geom_smooth(size=2, linetype = 3, se= FALSE, method = "lm") + facet_grid(.~ fips) + 
+labs(title = "PM2.5 emissions: Baltimore vs Los Angeles") +
+labs(x = expression("log"*PM[2.5]), y = "motor vehicles")
 
 ## I create the file.png
 
 png(filename = "plot6.png", width = 480, height = 480)
 
-g <- ggplot(NEI.cities, aes(Emissions, type))
-g + geom_point() + facet_grid(. ~ fips) + geom_smooth(method = "lm") + 
-labs(title = "Baltimore City vs Los Angeles County") + labs(x = "PM2.5 Emission", y = "Motor Vehicle Sources") 
+g <- ggplot(NEI.cities, aes(x=log(Emissions), y=type))
 
+g  +  geom_point(color = "steelblue", size = 4, alpha = 1/2) +  
+geom_smooth(size=2, linetype = 3, se= FALSE, method = "lm") + facet_grid(.~ fips) + 
+labs(title = "PM2.5 emissions: Baltimore vs Los Angeles") +
+labs(x = expression("log"*PM[2.5]), y = "motor vehicles")
 
 dev.off()
 
